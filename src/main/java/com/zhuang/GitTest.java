487487c7f984135d0b7946eaa4a3db7cf48bbfce
zhuang <1066078559@qq.com>
2022-01-07 15:19:07 +0800

    first commit

---
+++ src/main/java/com/zhuang/GitTest.java
@@ -0,0 +1,15 @@
+package com.zhuang;
+
+/**
+ * /**
+ *
+ * @program: git-test
+ * @description: git测试
+ * @author: Mr.Zhuang
+ * @create: 2022-01-06 11:50
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
